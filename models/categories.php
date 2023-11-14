22501e58298940c38caf1c061dc952e14d187248
chienvmph43391 <149553965+chienvmph43391@users.noreply.github.com>
2023-11-14 12:38:39 +0700

    BE-list category

---
+++ models/categories.php
@@ -0,0 +1,25 @@
+<?php
+function insert_category(){
+    $sql = "INSERT INTO `categories`(`category_id`, `category_name`) VALUES ('NULL','$category_name')";
+    pdo_execute($sql);
+}
+function loadall_category(){
+    $sql ="SELECT * FROM categories ORDER BY category_name ";
+    $listcategory= pdo_query($sql);
+    return $listcategory;
+}
+function delete_category(){
+    $sql = "DELETE FROM categories WHERE id=".$_GET['category_id'];
+    pdo_execute($sql);
+}
+function loadone_categories($id){
+    $sql = "SELECT * FROM categories WHERE id=".$category_id;
+    $category = pdo_query_one($sql);
+    return $category;
+}
+function update_categories($category_id , $category_name){
+    $sql = "UPDATE categories SET category_name='".$category_name."' WHERE id=".$category_id;
+    pdo_execute($sql);
+}
+
+?>
